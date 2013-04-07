4d398eb25dc60f6b22a48a9335c282ef084d2c10
Jakub Łopuszański <qbolec+git@gmail.com>
2013-04-07 10:26:40 +0200

    be consequent

--- Logic.h
+++ Logic.h
@@ -224,10 +224,38 @@ void foreachMatching(Graph &g,Callback &foo,unsigned int leftPos,Graph matching)
   if(leftPos == g.getLeftSize()){
     foo(matching);
   }else{
+    //be consequent:
+    int suggestedEndId = -1;
+    if(leftPos>0){
+      unsigned int prevNodeId = g.left(leftPos-1);
+      unsigned int myNodeId = g.left(leftPos);
+      unsigned int hisDegree =g.getOutDegree(prevNodeId);
+      unsigned int myDegree = g.getOutDegree(myNodeId);
+      if(0<matching.getOutDegree(prevNodeId) &&  myDegree==hisDegree){
+        vector<unsigned int> hisOptions;
+        for(unsigned int i=0;i<hisDegree;++i){
+          hisOptions.push_back(g.getOutEdgeEnd(prevNodeId,i)+1);
+        }
+        sort(hisOptions.begin(),hisOptions.end());
+        vector<unsigned int> myOptions;
+        for(unsigned int i=0;i<myDegree;++i){
+          myOptions.push_back(g.getOutEdgeEnd(myNodeId,i));
+        }
+        sort(myOptions.begin(),myOptions.end());
+        if(myOptions==hisOptions){
+          //be consequent, choose the same option.
+          suggestedEndId = matching.getOutEdgeEnd(prevNodeId,0)+1;
+        }        
+      }
+      
+    }
     unsigned int id=g.left(leftPos);
-    bool canSkip = true;
+    bool canSkip = suggestedEndId==-1;
     for(unsigned int i=0;i<g.getOutDegree(id);++i){
       unsigned int endId = g.getOutEdgeEnd(id,i);
+      if(suggestedEndId!=-1 && endId!=(unsigned int)suggestedEndId){
+        continue;
+      }
       if(0<matching.getOutDegree(endId)){
         unsigned int saveFromId = matching.getOutEdgeEnd(endId,0);
         matching.isolate(endId);
@@ -347,6 +375,10 @@ struct JudgeSticking{
   JudgeSticking(vector<Block> blocks):blocks(blocks),bestSticks(blocks.size(),blocks.size()),bestSticksCount(0){
   }
   void operator()(Graph sticks){
+<<<<<<< HEAD
+=======
+    //debugDumpAsHtml(sticks,"sticks");
+>>>>>>> be consequent
     unsigned int edgesCount = sticks.getEdgesCount();
     assert(edgesCount%2==0);
     if(bestSticksCount < edgesCount){
@@ -378,6 +410,10 @@ vector<Block> getBlocks(Graph matching){
   return blocks;
 }
 Graph getBestSticksForBlocks(vector<Block> blocks ){
+<<<<<<< HEAD
+=======
+  //debugDumpAsHtml(blocks);
+>>>>>>> be consequent
   JudgeSticking judge(blocks);
   foreachBlocksSticking(blocks,judge);
   return judge.getBestSticks();
@@ -440,7 +476,7 @@ struct ForEachWeak{
         leftOvers.isolate(id);      
       }
     }
-    //debugDumpAsHtml(leftOvers,"left overs");
+    debugDumpAsHtml(leftOvers,"left overs");
     ProcessMatching judge(strongMatching);
     foreachMatching(leftOvers,judge);
     Graph matching = judge.getBestMatching();
