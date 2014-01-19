0b5d0efad1bd4d5e405780ad759280bdfa2b4eae
Jakub Łopuszański <qbolec@gmail.com>
2014-01-19 19:16:11 +0100

    hadzia

--- Block.h
+++ Block.h
@@ -4,16 +4,16 @@ struct Block{
   unsigned int length;
   Block(unsigned int startSrc,unsigned int startDest,unsigned int length):
     startSrc(startSrc),startDest(startDest),length(length){};
-  unsigned int endSrc(){
+  unsigned int endSrc()const{
     return startSrc+length;
   }
-  unsigned int endDest(){
+  unsigned int endDest()const{
     return startDest+length;
   }
-  bool containsSrc(unsigned int src){
+  bool containsSrc(unsigned int src)const{
     return startSrc <= src && src<startSrc+length;
   }
-  bool containsDest(unsigned int dest){
+  bool containsDest(unsigned int dest)const{
     return startDest <= dest && dest<startDest +length;
   }
 };
--- Logic2.h
+++ Logic2.h
@@ -225,6 +225,7 @@ void debugDumpAsHtml(vector<Block> blocks){
   cerr << "</pre>";
 }
 bool areAligned(Block a,Block b){
+  assert(a.startSrc < b.startSrc);
   return a.startSrc < b.startSrc && a.startDest < b.startDest;
 }
 bool conflict(unsigned int aLo,unsigned int aHi,unsigned int bLo,unsigned int bHi){
@@ -237,52 +238,267 @@ bool conflict(Block aLo,Block aHi,Block bLo,Block bHi){
   return conflict(aLo.startSrc,aHi.startSrc,bLo.startSrc,bHi.startSrc) ||
     conflict(aLo.startDest,aHi.startDest,bLo.startDest,bHi.startDest);
 }
+bool isSubsetOf(vector<int> &A,vector<int> &B,int a,int b){
+  if(A.size()<=B.size()){
+    int bp=0;
+    for(int i=0;i<A.size();++i)if(A[i]!=b){
+      while(bp<B.size() && B[bp]<A[i]){
+        bp++;
+      }
+      if(bp>=B.size() || B[bp]!=A[i]){
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
+bool isOk(int v,const vector<int> &take,const vector<vector<int> > &originalGraph){
+  if(take[v]==-1){
+    FOREACH(n,originalGraph[v]){
+      if(take[*n]==1){
+        return true;
+      }
+    }
+    return false;
+  }
+  return true;
+}
+unsigned long long int notMaximal = 0;
 template<typename Callback>
-void foreachBlocksSticking(vector<Block> blocks,Callback &foo,unsigned int blockId,Graph sticks){
-  if(blockId == blocks.size()){
-    foo(sticks);
-  }else{
-    for(unsigned int prevBlockId=0;prevBlockId<blockId;++prevBlockId){
-      unsigned int from = sticks.left(blockId);
-            
-      if(areAligned(blocks[prevBlockId],blocks[blockId])){
-        unsigned int to =sticks.right(prevBlockId);             
-        if(!sticks.getOutDegree(to)){
-          bool stop = false;
-          for(unsigned int otherBlockId=0;otherBlockId<blockId;++otherBlockId){
-            unsigned int id=sticks.left(otherBlockId);
-            for(unsigned int j=0;j<sticks.getOutDegree(id);++j){
-              unsigned int endId = sticks.getOutEdgeEnd(id,j);
-              unsigned int otherBlockPrevId = sticks.whichRight(endId);
-              if(conflict(blocks[blockId],blocks[prevBlockId],blocks[otherBlockId],blocks[otherBlockPrevId])){
-                stop=true;
-              }
-            }
+void foreachMaximumIndependentSet(const vector<vector<int> > & originalGraph,vector<pair<int,int> > arcs,vector<vector<int> > nbrs, vector<int> take, Callback &foo,const Graph &sticks){
+
+  //Following heuristics makes sense in a general max independent set problem:
+  //if there is a node v, which is better from all it's neighbours in a sense, that 
+  //the set of nbrs of v is smaller than set of any nbr of v, then we can safely take v.
+  //This is perhaps easiest to see for a case where v is isolated :)
+  //Or for the case when v has a single nbr.
+  //But this can be generalised to degrees > 1 as described above.
+  //The problem with this heuristic in our situation is that we have some additional constraints in our problem.
+  //For example we want the matching brackets to be in the same component at the end of the day.
+  /*
+  unsigned int easy=0;
+  for(bool changed=true;changed;){
+    changed=false;
+    for(int v=0;v<nbrs.size();++v)if(!take[v]){
+      bool is_best=true;
+      FOREACH(nbr,nbrs[v]){
+        assert(!take[*nbr]);
+        if(!isSubsetOf(nbrs[v],nbrs[*nbr],v,*nbr)){
+          is_best=false;
+          break;
+        }
+      }
+      if(is_best){
+        take[v]=1;
+        FOREACH(nbr,nbrs[v]){
+          take[*nbr]=-1;
+          FOREACH(n,nbrs[*nbr])if(*n!=v){
+            nbrs[*n]=erase(nbrs[*n],*nbr);
           }
-          if(!stop){
-            sticks.addEdges(from,to);
-            foreachBlocksSticking(blocks,foo,blockId+1,sticks);
-            sticks.isolate(to);
+        }
+        easy++;
+        changed=true;
+        break;
+      }
+    }
+  }
+  //cerr << easy << "easy cases" << endl;
+  */
+  int best_v=-1;
+  for(int v=0;v<nbrs.size();++v)if(!take[v]){
+    if(best_v == -1 || nbrs[best_v].size() > nbrs[v].size()){
+      best_v=v;
+    }
+  }
+  if(best_v<0){
+    /*
+    for(int v=0;v<take.size();++v)if(take[v]==-1){
+      bool hasNbr=false;
+      FOREACH(n,originalGraph[v]){
+        if(take[*n]==1){
+          hasNbr=true;
+        }
+      }
+      if(!hasNbr){
+        //cerr << "Independent set is not maximal" << endl;
+        notMaximal++;
+        return;
+      }
+    }else{
+      assert(take[v]==1);
+      FOREACH(n,originalGraph[v]){
+        assert(take[*n]==-1);
+      }
+    }
+    */
+    //cerr << "found sticking" << endl;
+    Graph sticking=sticks;
+    for(int a=0;a<arcs.size();++a)if(take[a]==1){
+      //cerr << "Adding edge " << arcs[a].second << " ~> " << arcs[a].first << endl;
+      sticking.addEdges(sticking.left(arcs[a].second),sticking.right(arcs[a].first));
+    }
+    foo(sticking);
+  }else{
+    vector<int> options=nbrs[best_v];
+    options.push_back(best_v);
+    //cerr << "Iterating over " << options.size() << " Options" << endl;
+    //k-ta opcja polega na tym, że take[k]=1 i take[k+1]=take[k+2]=...=-1
+    for(int k=0;k<options.size();++k){
+      vector<int> newTake = take;
+      vector<vector<int> > newNbrs = nbrs;
+      int v= options[k];
+      //cerr << "Take " << v << endl;
+      newTake[v]=1;
+      bool isMaximal = true;
+      FOREACH(nbr,newNbrs[v]){
+        if(newTake[*nbr]){
+          cerr << k << ' ' << v << ' ' << *nbr << endl;;
+          FOREACH(u,newNbrs[v]){
+            cerr << *u << ' ';
+          }
+          cerr << endl;
+        }
+        assert(!newTake[*nbr]);
+        newTake[*nbr]=-1;
+        FOREACH(n,newNbrs[*nbr])if(*n!=v){
+          newNbrs[*n]=erase(newNbrs[*n],*nbr);
+          if(newNbrs[*n].empty() && !isOk(*n,newTake,originalGraph)){
+         //   cerr << "Do not go there" << endl;
+            isMaximal = false;
           }
         }
       }
+      for(int i=k+1;i<options.size();++i){
+        int nbr=options[i];
+        assert(newTake[nbr]!=1);
+        if(!newTake[nbr]){
+          newTake[nbr]=-1;
+          if(!isOk(nbr,newTake,originalGraph)){
+       //     cerr << "Dude do not go there" <<endl;
+            isMaximal = false;
+          }
+          FOREACH(n,newNbrs[nbr]){
+            newNbrs[*n]=erase(newNbrs[*n],nbr);
+            if(newNbrs[*n].empty() && !isOk(*n,newTake,originalGraph)){
+        //      cerr << "Srsly, do not go there" << endl;
+              isMaximal = false;
+            }
+          }
+        }
+      }
+      if(isMaximal){
+        foreachMaximumIndependentSet(originalGraph, arcs,newNbrs,newTake,foo,sticks);
+      }
+
     }
-    foreachBlocksSticking(blocks,foo,blockId+1,sticks);
   }
+
+}
+
+int getBBlock(unsigned int bp,const vector<Block> &blocks,const TextInfo & a){
+  for(unsigned int b=0;b<blocks.size();++b){
+    if(blocks[b].containsDest(bp+1+a.next.size())){
+      return b;
+    }
+  }
+  return -1;
+}
+int getABlock(unsigned int ap,const vector<Block> &blocks){
+  for(unsigned int b=0;b<blocks.size();++b){
+    if(blocks[b].containsSrc(ap+1)){
+      return b;
+    }
+  }
+  return -1;
 }
 
 template<typename Callback>
-void foreachBlocksSticking(vector<Block> blocks,Callback &foo){
+void foreachBlocksSticking(vector<Block> blocks,Callback &foo,TextInfo &a,TextInfo &b){
+  vector<pair<int,int> > implicitArcs;
+  for(unsigned int i=0;i<a.next.size();++i){
+    if(a.next[i]<a.next.size()){
+      int pb=getABlock(i,blocks);
+      int nb=getABlock(a.next[i],blocks);
+      if(pb!=nb){
+        assert(pb<nb);
+        assert(blocks[pb].startSrc < blocks[nb].startSrc);
+        if(areAligned(blocks[pb],blocks[nb])){
+          implicitArcs.push_back(make_pair(pb,nb));
+        }else{
+          cerr << "The whole matching has wrongly matched (unaligned) brackets" << endl;
+          assert(false);//This should be checked one level up
+          return;
+        }
+      }
+    }
+  }
+  for(unsigned int i=0;i<b.next.size();++i){
+    if(b.next[i]<b.next.size()){
+      int pb=getBBlock(i,blocks,a);
+      int nb=getBBlock(b.next[i],blocks,a);
+      if(pb!=nb){
+        assert(pb<nb);
+        assert(blocks[pb].startDest < blocks[nb].startDest);
+        if(areAligned(blocks[pb],blocks[nb])){
+          implicitArcs.push_back(make_pair(pb,nb));
+        }else{
+          cerr << "The whole matching has wrongly matched (unaligned) brackets" << endl;
+          assert(false);//This should be checked one level up
+          return;
+        }
+      }
+    }
+  }
+  vector<pair<int,int> > arcs;
+  for(int i=blocks.size();i--;){
+    for(int j=blocks.size();j--;)if(i<j){
+      assert(blocks[i].startSrc < blocks[j].startSrc);
+      if(areAligned(blocks[i],blocks[j])){
+        bool bad=false;
+        FOREACH(imp,implicitArcs){
+          if(conflict( blocks[imp->second],blocks[imp->first], blocks[j],blocks[i])){
+            bad=true;
+          }
+        }
+        if(!bad){
+          arcs.push_back(make_pair(i,j));
+        }
+      }
+    }
+  }
+  vector<vector<int> > nbrs(arcs.size());
+  int edges=0;
+  for(int i=arcs.size();i--;){
+    for(int j=i;j--;){
+      if(conflict( blocks[arcs[i].second],blocks[arcs[i].first],blocks[arcs[j].second],blocks[arcs[j].first]) || arcs[i].second==arcs[j].second || arcs[i].first==arcs[j].first){
+        edges++;
+        nbrs[i].push_back(j);
+        nbrs[j].push_back(i);
+      }
+    }
+  }
+  FOREACH(n,nbrs){
+    sort(n->begin(),n->end());
+  }
+  notMaximal = 0;
+  //cerr << "arcs " << arcs.size() << " edges " << edges << endl;
   Graph sticks(blocks.size(),blocks.size());
-  foreachBlocksSticking(blocks,foo,0,sticks);
+  foreachMaximumIndependentSet(nbrs,arcs,nbrs,vector<int> (arcs.size(),0),foo,sticks);
+  //cerr << "Not maximal sets " << notMaximal << endl;
+  //foreachBlocksSticking(blocks,foo,0,sticks);
 }
+
+
 struct JudgeSticking{
   vector<Block> blocks;
   TextInfo a;
   TextInfo b;
   Graph bestSticks;
   unsigned int bestSticksCount;
-  JudgeSticking(vector<Block> blocks,TextInfo a,TextInfo b):blocks(blocks),a(a),b(b),bestSticks(blocks.size(),blocks.size()),bestSticksCount(0){
+  unsigned long long int stickingsTested;
+  unsigned long long int validBrackets;
+  JudgeSticking(vector<Block> blocks,TextInfo a,TextInfo b):blocks(blocks),a(a),b(b),bestSticks(blocks.size(),blocks.size()),bestSticksCount(0),stickingsTested(0),validBrackets(0){
   }
   int getBBlock(unsigned int bp){
     for(unsigned int b=0;b<blocks.size();++b){
@@ -314,16 +530,20 @@ struct JudgeSticking{
     return areBlocksConnected(sticks.whichLeft(sticks.getOutEdgeEnd(sticks.right(ab),0)),bb,sticks);
   }
   void operator()(Graph sticks){
+    stickingsTested++;
     for(unsigned int i=0;i<a.next.size();++i){
       if(a.next[i]<a.next.size()){
         int pb=getABlock(i);
         int nb=getABlock(a.next[i]);
+        //cerr << "Verifying " << i << " (" << pb << ") paired with " << a.next[i] << " (" << nb << ")" << endl;
         
         if((-1<=pb) != (-1<=nb)){
           return;
         }
         if(-1<=pb && -1<=nb){
+          //cerr << "Both are matched" << endl;
           if(!areBlocksConnected(pb,nb,sticks)){
+            //cerr << "Brackets are not correctly connected" << endl;
             return;
           }
         }
@@ -343,6 +563,7 @@ struct JudgeSticking{
         }
       }
     }
+    validBrackets ++;
 
     //debugDumpAsHtml(sticks,"sticks");
     unsigned int edgesCount = sticks.getEdgesCount();
@@ -379,7 +600,8 @@ vector<Block> getBlocks(Graph matching){
 Graph getBestSticksForBlocks(vector<Block> blocks,TextInfo a,TextInfo b ){
   //debugDumpAsHtml(blocks);
   JudgeSticking judge(blocks,a,b);
-  foreachBlocksSticking(blocks,judge);
+  foreachBlocksSticking(blocks,judge,a,b);
+  //cerr << "Blocks count " << blocks.size() << ", stickings tested " << judge.stickingsTested << " of which " << judge.validBrackets << " have valid brackets" << endl ;
   return judge.getBestSticks();
 }
 Graph getBestSticksForMatching(Graph matching,TextInfo a,TextInfo b){
@@ -749,7 +971,8 @@ struct OnEachBlocksSequence{
   Graph bestMatching;
   unsigned int bestCost;
   unsigned long long testedSequences;
-  OnEachBlocksSequence(TextInfo &a,TextInfo &b):a(a),b(b),bestMatching(a.important_text.length(),b.important_text.length()),bestCost(1e9),testedSequences(0){
+  unsigned long long validBrackets;
+  OnEachBlocksSequence(TextInfo &a,TextInfo &b):a(a),b(b),bestMatching(a.important_text.length(),b.important_text.length()),bestCost(1e9),testedSequences(0),validBrackets(0){
   }
   void onBlocksSequence(const vector<vector<pair<int,int> > > &blocks){
     testedSequences++;
@@ -776,6 +999,7 @@ struct OnEachBlocksSequence{
         }
       }
     }
+    validBrackets++;
     //debugDumpAsHtml(matching,"Candidate matching");
     unsigned int currentCost = cost(matching,a,b);
     //cerr << "cost of this matching is " << currentCost << endl;
@@ -1397,7 +1621,7 @@ Graph getBestMatching(TextInfo a,TextInfo b){
   vector<pair<int,int> > conflictingBlocks = getConflictingBlocks(dominantBlocks);
   OnEachBlocksSequence visitor(a,b);
   foreachBlocksSequence(dominantBlocks,conflictingBlocks,visitor);
-  cerr << "Tested " << visitor.testedSequences << " sequences!" << endl;
+  cerr << "Tested " << visitor.testedSequences << " sequences of which " << visitor.validBrackets << " have valid brackets " << endl;
   return visitor.bestMatching;
 }
 void getCheapest(string aText,string bText,DFA &dfa){
